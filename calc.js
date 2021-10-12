079d029ea5871902da18d625e9b814595167d8a4
Timothy Luebke <luebketj@gmail.com>
2021-10-12 14:52:33 -0600

    added functionality for Addition

--- calc.js
+++ calc.js
@@ -14,8 +14,14 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
-
+	console.log("mathSymbol ", mathSymbol);
+	console.log("num1 ", num1);
+	console.log("num2 ", num2);
+	if(mathSymbol == "+"){
+		console.log("sum " + String(num1 + num2));
+	}
 	// This line closes the connection to the command line interface.
+
 	reader.close()
 
 });
