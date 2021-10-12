854d7fe484861fa09ae7ed22f25758458cfb133d
Timothy Luebke <luebketj@gmail.com>
2021-10-12 15:04:28 -0600

    Added functionality for division

--- calc.js
+++ calc.js
@@ -14,15 +14,21 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	//Log out the variables so I know what 
+
 	console.log("mathSymbol ", mathSymbol);
 	console.log("num1 ", num1);
 	console.log("num2 ", num2);
+
+
 	if(mathSymbol == "+"){
 		console.log("sum " + String(num1 + num2));
 	}else if (mathSymbol == "-"){
         console.log("sum " + String(num1 - num2));
     }else if (mathSymbol == "*"){
         console.log("sum " + String(num1 * num2));
+    }else if(mathSymbol == "/"){
+        console.log("sum " + String(num1 / num2));
     }
 	// This line closes the connection to the command line interface.
 
